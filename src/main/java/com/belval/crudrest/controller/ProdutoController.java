f280e9ca726b34c9d146f072778da253284cbe93
Alessandro Tacini <alessandro.tacini@usp.br>
2024-09-20 17:26:25 -0300

    Ajustes

--- src/main/java/com/belval/crudrest/controller/ProdutoController.java
+++ src/main/java/com/belval/crudrest/controller/ProdutoController.java
@@ -14,24 +14,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.belval.crudrest.model.Produto;
-import com.belval.crudrest.repository.ProdutoRepository2;
+import com.belval.crudrest.repository.ProdutoRepository;
 
 @RestController
 public class ProdutoController {
 
-//	private static List<Produto> listaProdutos = new ArrayList<>();
-//	private static Integer proxId = 1;
-	
-//	private static ProdutoRepository repository = 
-//			new ProdutoRepository();
 	@Autowired
-	private ProdutoRepository2 repository;
-	
-
-	static {
-//		Produto prod = new Produto(1, "Pão", "Pão Francês", 0.5);
-//		listaProdutos.add(prod);
-	}
+	//private ProdutoRepositoryInMemory repository;
+	private ProdutoRepository repository;
 	
 	@GetMapping("/produtos")
 	public ResponseEntity<Iterable<Produto>> obterProdutos() {
